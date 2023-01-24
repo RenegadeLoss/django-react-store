d925e1c5f4575fe18e4cc59004bb9c344aeb0cc1
RenegadeLoss <renegadeloss696@gmail.com>
2023-01-24 19:37:23 +0300

    Change DB url

--- backend/settings.py
+++ backend/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/drcatalog', conn_max_age=600)
+    'default': dj_database_url.config(default='postgres://renegade:MJ0Ys6KepwE0hNLkmv0ZKmcOt5GiIUIZ@dpg-cf3c4garrk0bppdk8sjg-a/drcatalog', conn_max_age=600)
 }
 
 
